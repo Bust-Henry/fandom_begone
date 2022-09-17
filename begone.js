fa09f1deae3cde00e7b2de78991a46adcf33fad4
Henry Bust <Henry.Bust@hotmail.de>
2022-09-17 23:27:21 +0200

    first commit

---
+++ begone.js
@@ -0,0 +1,11 @@
+$(function(){
+    console.log("fuckoff fandom bs");
+    $('.global-navigation').remove();
+    $('.top-ads-container').remove();
+    $('.bottom-ads-container').remove();
+    $('#mixed-content-footer').remove();
+    $('.global-footer').remove();
+    $('#WikiaBar').remove();
+    $('.main-container').removeClass('main-container');
+    $(".fandom-sticky-header").css("left", "0");
+});
